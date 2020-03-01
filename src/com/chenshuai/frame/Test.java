cf24ccfe620313847949b84b0b68566edee2e2a6
chenshuai <chenshuai5@asiainfo.com>
2020-03-01 11:48:31 +0800

    提交代码测试

---
+++ src/com/chenshuai/frame/Test.java
@@ -0,0 +1,13 @@
+package com.chenshuai.frame;
+
+public class Test {
+     private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
